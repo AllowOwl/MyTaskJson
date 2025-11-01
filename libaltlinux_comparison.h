f313a8b0c93ee49f2e77bf8fae3d80b886c8c644
AllowOwl <2016scoyvoker@mail.ru>
2025-11-01 21:52:43 +0300

    Added  methods to the library

---
+++ libaltlinux_comparison.h
@@ -0,0 +1,32 @@
+#ifndef LIBALTLINUX_COMPARISON_H
+#define LIBALTLINUX_COMPARISON_H
+
+#include <string>
+#include <nlohmann/json.hpp>
+
+namespace altlinux {
+    class PackageComparator {
+        public:
+            nlohmann::json compare_branches(
+                const std::string& branch_1,
+                const std::string& branch_2,
+                const std::string& arch = "");
+
+            nlohmann::json load_branch_packages(
+                const std::string& branch,
+                const std::string& arch = ""
+            );
+        
+        private:
+            std::string api_base_url_ = "https://rdb.altlinux.org/api";
+
+            nlohmann::json compare_packages_by_arch(
+                const nlohmann::json& branch_1_pk,
+                const nlohmann::json& branch_2_pk
+            );
+
+            bool version_greater(const std::string& ver1, const std::string& ver2);
+    };
+}
+
+#endif
